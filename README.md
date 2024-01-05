ba9edec90a45e7babca60afdd6dfe418d96232aa
Damien PORTÉ <damien.porte@soprasteria.com>
2024-01-05 01:09:43 +0100

    feat: Adding unit test project template with unit test sample of the template.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DotnetTemplates
 Buch of templates based on the dotnet new command.
+
+# Ressources
+- https://github.com/dotnet/templating/wiki
---
+++ Templates.Tests/Class1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.TemplateEngine.Authoring.TemplateVerifier;
+
+namespace Templates.Tests
+{
+	public class MyTestClass
+	{
+		[Fact]
+		public async Task MyTemplate_InstantiationTest()
+		{
+			ServiceProvider serviceProvider = new ServiceCollection()
+				.AddLogging(builder => builder.AddDebug())
+				.BuildServiceProvider();
+
+			ILoggerFactory? factory = serviceProvider.GetService<ILoggerFactory>();
+
+			ILogger<MyTestClass> logger = factory.CreateLogger<MyTestClass>();
+
+			TemplateVerifierOptions options = new(templateName: "ut")
+			{
+				TemplateSpecificArgs = new[] { "-tf", "net6.0" },
+				VerifyCommandOutput = false,
+			};
+
+			VerificationEngine engine = new(logger);
+			await engine.Execute(options).ConfigureAwait(false);
+		}
+	}
+}
